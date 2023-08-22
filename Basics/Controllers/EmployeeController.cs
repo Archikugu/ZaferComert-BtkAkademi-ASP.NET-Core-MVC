029a796ba1766b1ceda0a188724bb6ea8358f572
Gokhan <engokhangok@gmail.com>
2023-08-22 19:18:35 +0300

    Add project files.

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public String Index()
+        {
+            return "Hello World!";
+        }
+        public IActionResult Index1()
+        {
+            string message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message);
+        }
+        public ViewResult Index2()
+        {
+            var names = new String[]
+            {
+                "Ahmet",
+                "Mehmet",
+                "Can"
+            };
+            return View(names);
+        }
+        //public IActionResult Index3()
+        //{
+        //    return Content("Employee");
+        //}
+        public IActionResult Index3()
+        {
+            var list = new List<Employee>()
+            {
+                new Employee() {ID=1,FirstName="Gökhan",LastName="Gök",Age=23},
+                new Employee() {ID=2,FirstName="Can",LastName="Dağ",Age=20},
+                new Employee() {ID=3,FirstName="Demir",LastName="Güneş",Age=37},
+            };
+            return View("Index3", list);
+        }
+    }
+}
---
+++ Basics/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Basics.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public String FirstName { get; set; } = string.Empty;
+        public String LastName { get; set; } = string.Empty;
+        public String FullName => $"{FirstName} {LastName.ToUpper()}";
+        public int Age { get; set; }
+
+    }
+}
